4047714e6907368ceb7b1993d9cd808ac0f9f344
william chou <w.chou06@gmail.com>
2017-02-20 17:42:58 -0500

    implemented text turning to red if negative.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,14 @@ $(() => {
   $("textarea").keypress(function () {
     // console.log($(".counter").innerHTML = 140 - this.value.length);
     //console.log($(".counter"));
-    $(".counter").html(140 - this.value.length);
+    let counter = $(".counter");
+    console.log($(this).val());
+    if ($(this).val().length > 140) {
+      counter.html(140 - this.value.length);
+      counter.css("color", "red");
+    } else {
+      counter.html(140 - this.value.length);
+      counter.css("color", "black");
+    }
   });
 });
