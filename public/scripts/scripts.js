04d27a3d4cdeb0b4f53075d79061494c62363a3e
william chou <w.chou06@gmail.com>
2017-02-21 13:28:51 -0500

    Implemented adding icons on hover.

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
   //implementing this post:
-  //http://stackoverflow.com/questions/28452235/make-a-nav-bar-stick-to-the-top-when-scrolling-with-css
-  $(window).scroll(function () {
-    //if you hard code, then use console
-    //.log to determine when you want the 
-    //nav bar to stick.  
-    console.log($(window).scrollTop()) //logs the position at the top of the page
-      // if ($(window).scrollTop() > 80) {
-      //   $('#nav_bar').addClass('navbar-fixed');
-      // }
-      // if ($(window).scrollTop() < 81) {
-      //   $('#nav_bar').removeClass('navbar-fixed');
-      // }
-  });
+  $(".card")
+    .hover(
+      //hover in function
+      //can only use fat arrow if I look at the event, this would return the entire document.
+      function () {
+        //console.log(this);
+        $(this).find('.card-footer').append(
+          `<span class="tweet-icons">
+          <a href="#"><i class="material-icons">flag</i></a>
+          <a href="#"><i class="material-icons">repeat</i></a>
+          <a href="#"><i class="material-icons">favorite</i></a>
+          </span>`
+        );
+      },
+      //hover out function
+      function () {
+        // console.log(this);
+        $(this).find(".tweet-icons").remove();
+      }
+    );
 });
