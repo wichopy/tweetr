5f801a051af0f499930b9b86035bb701315d11e0
william chou <w.chou06@gmail.com>
2017-02-22 14:02:18 -0500

    successfully implemented mongodb!!

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,10 +1,7 @@
 "use strict";
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
-
-// Simulates the kind of delay we see with network or filesystem operations
-// const simulateDelay = require("./util/simulate-delay");
+//USED THIS TO GET MY MONGO RUNNING!
+//https://zellwk.com/blog/crud-express-mongodb/
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -15,24 +12,34 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     // Saves a tweet to `db`
     saveTweet: function (newTweet, callback) {
-
-
-
-
-      saveTweet(newTweet, (err) => {
-        if (err) throw err;
-        db.close();
+      //returns value in callback.
+      db.collection("tweets").insertOne(newTweet, (err, result) => {
+        if (err) {
+          return callback(err);
+        }
+        console.log('successful save!');
+        callback(null, true);
       });
-
     },
-
     // Get all tweets in `db`, sorted by newest first
     getTweets: function (callback) {
-
-
+      function getTweets(callback) {
+        db.collection("tweets").find().toArray((err, tweets) => {
+          if (err) {
+            return callback(err);
+          }
+          callback(null, tweets);
+        });
+      }
+      getTweets(function (err, tweets) {
+        if (err) callback(err);
+        //console.log('grabbed tweets!');
+        // console.log(tweets);
+        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        callback(null, tweets.sort(sortNewestFirst)); //Call parent callback function.
+      });
       //for sorting the db from newest to oldest
-      const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-      callback(null, db.tweets.sort(sortNewestFirst));
+
 
     }
 
