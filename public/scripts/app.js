3e50c21b9ae5050f423befc1fada915c8a5f0415
william chou <w.chou06@gmail.com>
2017-02-22 17:41:49 -0500

    slide toggle, fixed char counter function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,68 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
-
-var data = [{
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1487706307431
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 31113796368
-  }
-];
 
 
 function timeAgo(timestamp) {
@@ -130,17 +68,20 @@ function loadTweets() {
     });
 }
 
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+// function escape(str) {
+//   var div = document.createElement('div');
+//   div.appendChild(document.createTextNode(str));
+//   return div.innerHTML;
+// }
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function () {
   loadTweets();
   $('.compose-btn').on('click', function () {
-    console.log("OPen and Close compose.");
+    var composebox = $(".new-tweet");
+    composebox.slideToggle();
+    composebox.find('textarea').focus();
+
   });
   //need to use dynamic listener!
   $('#tweet-cards').on('mouseenter', '.card',
@@ -168,38 +109,35 @@ $(document).ready(function () {
   //var $tweet = createTweetElement(tweetData);
   $("form").on("submit", function (event) {
     event.preventDefault(); //doesn;t let browser do its actions.
-    // console.log($(this));
-    // if (!$(this).val()) {
-    //   console.log("error blank textarea.");
-    //   throw new Error("blank page!");
-    //   window.alert("blank box!");
-    // }
-    //console.log($('.new-tweet textarea').val());
+    console.log($(this).find('textarea').val().length);
+    if (!$(this).find('textarea').val()) {
+      console.log("Print me if blank");
+      alert("Put something in before you tweet!");
+    } else if ($(this).find('textarea').val().length > 140) {
+      console.log("Too long!!");
+      console.log($(this).find('textarea').val());
+      alert("too long, please decrease your tweet length");
+    } else {
 
-    // console.log($(this[0].value));
-    // console.log($(this).serialize());
-    // console.log($(this));
-    // console.log(escape("<script>"));
-    //console.log($(this).find('textarea').val());
-    //ESCAPE form data!
-    $(this).find('textarea').val(escape($(this).find('textarea').val()));
-    let textdata = $(this).serialize();
-    $.ajax({
-        url: '/tweets/',
-        method: 'POST',
-        data: textdata
+      //ESCAPE form data!
+      $(this).find('textarea').val(escape($(this).find('textarea').val()));
+      let textdata = $(this).serialize();
+      $.ajax({
+          url: '/tweets/',
+          method: 'POST',
+          data: textdata
 
-      })
-      .then((data) => {
-        // console.log("successful send");
-        // console.log(data);
-        // var newtweet = createTweetElement(data)
-        // console.log(newtweet);
-        // $('#tweet-cards').prepend();
-        $('.new-tweet').find('textarea').val(""); // clear text box after successful post.
-        loadTweets(); //rerender new tweets when finished.
-      });
+        })
+        .then((data) => {
+          // console.log("successful send");
+          // console.log(data);
+          // var newtweet = createTweetElement(data)
+          // console.log(newtweet);
+          // $('#tweet-cards').prepend();
+          $('.new-tweet').find('textarea').val(""); // clear text box after successful post.
+          loadTweets(); //rerender new tweets when finished.
+        });
 
-
+    }
   });
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -18,4 +18,22 @@ $(() => {
       }
     }
   });
+  $("textarea").keydown(function () {
+    // console.log($(".counter").innerHTML = 140 - this.value.length);
+    //console.log($(".counter"));
+    let counter = $(".counter");
+    if ($(this).val().length > 140) {
+      counter.addClass("negcount");
+      counter.html(140 - this.value.length);
+      if (counter.hasClass("poscount")) {
+        counter.removeClass("poscount");
+      }
+    } else {
+      counter.addClass("poscount");
+      counter.html(140 - this.value.length);
+      if (counter.hasClass("negcount")) {
+        counter.removeClass("negcount");
+      }
+    }
+  });
 });
